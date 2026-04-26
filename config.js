20705855094a12bd836fc60a755839d1f0944bad
DominicDev <s0l0estudi4r@gmail.com>
2026-04-26 23:17:26 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'SYSTEM OF A DOWN';
+export let nameSongs = [
+	"CHOP SUEY!", /*1*/
+	"TOXICITY", /*2*/
+	"AERIALS", /*3*/
+	"HOLY MOUNTAINS", /*4*/
+	"REVENGA LYRICS", /*5*/
+	"RADIO/VIDEO", /*6*/
+	"BYOB", /*7*/
+	"SAD STATUE", /*8*/
+	"DREAMING", /*9*/
+	"TENTATIVE LYRICS", /*10*/
+	"A.D.D", /*11*/
+	"WAR?", /*12*/
+	"MIND", /*13*/
+	"SOIL", /*14*/
+	"NEEDLES", /*15*/
+	"PRISON SONG", /*16*/
+	"QUESTION!", /*17*/
+	"LOST IN HOLLYWOOD", /*18*/
+	"LONELY DAY", /*19*/
+	"SNOWBLIND", /*20*/
+]
